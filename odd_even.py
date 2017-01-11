2143e26ca700b09eef3a53da280bc54c587d0508
kristaprokop <krista.prokop@gmail.com>
2017-01-11 11:57:29 -0600

    odd and even conditionals

---
+++ odd_even.py
@@ -0,0 +1,15 @@
+# Create a function that counts from 1 to 2000. As it loops through each number, have your program generate the number and specify whether it's an odd or even number.
+
+# Your program output should look like below:
+
+# Number is 1.  This is an odd number.
+# Number is 2.  This is an even number.
+# Number is 3.  This is an odd number.
+# ...
+# Number is 2000.  This is an even number.
+
+for count in range(1, 2001):
+  if count % 2 == 0:
+    print "Number is", str(count)+'.', "This is an even number."
+  else:
+     print "Number is", str(count)+'.', "This is an odd number."
