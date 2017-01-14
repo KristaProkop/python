222af306aa6ab2006e5b172e734d8d1748cbcee5
kristaprokop <krista.prokop@gmail.com>
2017-01-13 18:17:03 -0600

    Algorithm and crawler

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,21 @@
-my_list = [6, 5, 3, 1, 8, 7, 2, 4]
-
+# my_list = [6, 5, 3, 1, 8, 7, 2, 4]
 #expected result: [5, 3, 1, 6, 7, 2, 4, 8]
 
-for i in range( (len(my_list)-1) ):
-    if my_list[i] > my_list[i+1]:
-        my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
-print my_list
+import random 
+from datetime import datetime
+
+startTime = datetime.now()
+
+
+def bubble_sort(my_list):
+    print "original list:\n", my_list, "\n"
+    for i in range( (len(my_list)-1) ):
+        if my_list[i] > my_list[i+1]:
+            my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
+    return my_list
+
+random_list = [int(10000*random.random()) for i in xrange(100)]
+print(bubble_sort(random_list))
+
+print "\n Process execution time: ", (datetime.now() - startTime)
+
---
+++ names.py
@@ -0,0 +1,49 @@
+# Create a program that outputs:
+# Michael Jordan
+# John Rosales
+# Mark Guillen
+# KB Tonel
+
+
+students = [ 
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+for items in students:
+  print items.values()[0], items.values()[1]
+
+#--------------------------------------------------
+# Create a program that outputs:
+# Students
+# 1 - MICHAEL JORDAN - 13
+# 2 - JOHN ROSALES - 11
+# 3 - MARK GUILLEN - 11
+# 4 - KB TONEL - 7
+# Instructors
+# 1 - MICHAEL CHOI - 11
+# 2 - MARTIN PURYEAR - 13
+
+
+users = {
+ 'Students': [ 
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for key, data in users.items():
+     print key
+     counter = 1
+     for value in data:
+       full_name = value["first_name"] + " " + value["last_name"]
+       print counter, "-", full_name.upper(), "-", len(str(full_name))-1 #exclude space from character count
+       counter+=1
