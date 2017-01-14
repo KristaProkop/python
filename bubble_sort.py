09ce862982b8dabd1b96bcfdcd82a6f0ed907906
kristaprokop <krista.prokop@gmail.com>
2017-01-14 13:03:49 -0600

    Sorting algorithms

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,5 @@
 # my_list = [6, 5, 3, 1, 8, 7, 2, 4]
-#expected result: [5, 3, 1, 6, 7, 2, 4, 8]
+
 
 import random 
 from datetime import datetime
@@ -19,3 +19,16 @@ print(bubble_sort(random_list))
 
 print "\n Process execution time: ", (datetime.now() - startTime)
 
+
+#here's another way of doing it:
+#expected result: [5, 3, 1, 6, 7, 2, 4, 8]
+
+def bubble_sort2(try_list):
+    for i, j in enumerate(try_list):
+        if try_list[i] < try_list[i-1]:
+            try_list.remove(j)
+            try_list.insert(i-1, j)
+    return try_list
+
+try_list = [6, 5, 3, 1, 8, 7, 2, 4]
+print(bubble_sort2(try_list))
