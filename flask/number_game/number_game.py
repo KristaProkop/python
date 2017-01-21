4491e425f8037f1475195210a23bf498c90b5afd
kristaprokop <krista.prokop@gmail.com>
2017-01-21 16:05:42 -0600

    Refactor and add play again button

--- flask/number_game/number_game.py
+++ flask/number_game/number_game.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template,request, redirect, session
 import random 
 
-##TODO: Put 'Play again' button in a form and make the button work
-
 app = Flask(__name__)
 app.secret_key = "secretkey"
 
@@ -18,26 +16,28 @@ def index():
 
 @app.route('/', methods=['POST'])
 def guesses():
-    session['counter'] += 1
-    guess = request.form['guess']
-    if session['num'] == int(guess):
-        session['result'] = "Correct!"
-        session['color'] = 'green'
-        session['counter'] = 0
-    else: 
-        if session['counter'] < 3:
-            if session['num'] > int(guess):
-                session['result'] = "too low!"
-            else:
-                session['result'] = "too high!"
-        if session['counter'] == 3:
-            session['result'] = "Incorrect. The number was " + str(session['num'])
-            session['counter'] = 0
+    if request.form['btn'] == 'Play Again':
+        return index()
+    if request.form['btn'] == 'Submit':
+        session['counter'] += 1
+        guess = request.form['guess']
+        if session['num'] == int(guess):
+            session['result'] = "Correct!"
+            session['color'] = 'green'
             session['playagain'] = 'show'
             session['formsubmit'] = 'hide'
-        session['color'] = 'red'
-    return render_template('index.html')
-
+        else: 
+            if session['counter'] < 10:
+                if session['num'] > int(guess):
+                    session['result'] = "too low!"
+                else:
+                    session['result'] = "too high!"
+            if session['counter'] == 10:
+                session['result'] = "Sorry! The number was " + str(session['num'])
+                session['playagain'] = 'show'
+                session['formsubmit'] = 'hide'
+            session['color'] = 'red'
+        return render_template('index.html')
 
 
 app.run(debug=True) 
