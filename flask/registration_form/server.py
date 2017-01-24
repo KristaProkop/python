c239eddef7236aca61330c3f8eefaefcc508cf2a
kristaprokop <krista.prokop@gmail.com>
2017-01-23 19:39:48 -0600

    Add validation for date

--- flask/registration_form/server.py
+++ flask/registration_form/server.py
@@ -1,24 +1,42 @@
 from flask import Flask, redirect, request, render_template, session, flash
+import re
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.secret_key = 'secretkey'
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/result', methods=['POST'])
-def create_student():
-    if len(request.form['first_name']) < 1 or len(request.form['last_name']) < 1 or len(request.form['email']) < 1 or len(request.form['password1']) < 1 or len(request.form['password2']) < 1:
+@app.route('/validate', methods=['POST'])
+def validation():
+    first_name = str(request.form['first_name'])
+    last_name = str(request.form['last_name'])
+    email = str(request.form['email'])
+    password1 = str(request.form['password1'])
+    password2 = str(request.form['password2'])
+    dob = str(request.form['dob'])
+    try:
+        datetime.strptime(dob, "%Y-%m-%d")
+        # TODO: add check for date in the past
+    except:
+        flash("Please enter date in exact format: yyyy-mm-dd")
+        return redirect('/')
+    if len(first_name) < 1 or len(last_name) < 1 or len(email) < 1 or len(password1) < 1 or len(password2) < 1:
         flash("All fields are required. Please complete all fields and resubmit.")
+    elif any(filter(str.isdigit, first_name)) or any(filter(str.isdigit, last_name)):
+        flash("Names cannot contain numbers. Please try again.")
+    elif len(password1) < 9:
+        flash("Password must be 8 or more characters")
+    elif password1 != password2:
+        flash("Passwords must match")
+    elif not EMAIL_REGEX.match(request.form['email']):
+        flash("Invalid Email Address!")
     else:
-        flash("Success! your name is {}".format(request.form['name']))
+        flash("Success! Thanks for registering, {}".format(first_name))
     return redirect('/')
    
 app.run(debug=True) 
-
-# All fields are required and must not be blank
-# First and Last Name cannot contain any numbers
-# Password should be more than 8 characters
-# Email should be a valid email
-# Password and Password Confirmation should match
