dac0c55142d86dbcebe32247d50fb49322f040fd
kristaprokop <krista.prokop@gmail.com>
2017-02-13 18:12:29 -0400

    Bike class

---
+++ oop/bike.py
@@ -0,0 +1,46 @@
+# Have the second instance ride twice, reverse twice and have it displayInfo(). Have the third instance reverse three times and displayInfo().
+
+class Bike(object):
+  def __init__(self, price, max_speed, miles=0):
+    print "new bike"
+    self.price = price
+    self.max_speed = max_speed
+    self.miles = miles
+
+  def displayInfo(self):
+    print "Max speed is", self.max_speed
+    print "Price is", self.price
+    print "Total miles =", self.miles
+
+  def ride(self):
+    print "riding"
+    self.miles = self.miles + 10
+
+  def reverse(self):
+    print "Reversing"
+    if self.miles <= 5:
+      self.miles = 0
+    else:
+      self.miles = self.miles - 5
+
+
+bike1 = Bike(200, "25mph")
+bike2 = Bike(250, "40mph")
+bike3 = Bike(100, "10mph")
+
+for x in range(0, 3):
+  bike1.ride()
+bike1.reverse()
+bike1.displayInfo()
+
+for x in range(0, 2):
+  bike2.ride()
+  bike2.reverse()
+bike2.displayInfo()
+
+for x in range(0, 3):
+  bike3.reverse()
+bike3.displayInfo()
+
+
+
