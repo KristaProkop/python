a2420d8004a47aa0a27293b817255b42d4ed5e4b
kristaprokop <krista.prokop@gmail.com>
2017-02-21 14:33:49 -0400

    Refactor to fix subtract function

--- oop/mathdojo.py
+++ oop/mathdojo.py
@@ -1,54 +1,38 @@
-#subtract function not working
-
 class MathDojo(object):
   def __init__(self):
-    print "Calculating..."
     self.result = 0
 
-  def add(self, num1, *num2):
-    if type(num1) == list or type(num1) == tuple:
-      for num in num1:
+  def add(self, item1, *item2):
+    if type(item1) == list or type(item1) == tuple:
+      for num in item1:
         self.result += num
     else:
-      self.result += num1
-    if num2:
-      if type(num2) == list or type(num2) == tuple:
-        for num in num2:
+      self.result += item1
+    if item2:
+      for num in item2:
+        if type(num) == list or type(num) == tuple:
+          for subItem in num:
+            self.result += subItem
+        else:
           self.result += num
-      else:
-        self.result += num2
     return self
-    
-  def subtract(self, num1, *num2):
-    if type(num1) == list or type(num1) == tuple:
-      subtotal = 0
-      for num in num1:
-        subtotal += num
-        print subtotal
-      self.result -= subtotal
+
+  def subtract(self, item1, *item2):
+    if type(item1) == list or type(item1) == tuple:
+      for var in item1:
+        self.result -= var
     else:
-      self.result = num1
-    if num2:
-      if type(num2) == list or type(num2) == tuple:
-        subtotal = 0
-        for num in num2:
-          print num
-          subtotal += num 
-        self.result -= subtotal
-      else:
-        self.result -= num2
+      self.result -= item1
+    if item2:
+      for num in item2:
+        if type(num) == list or type(num) == tuple:
+          for var in num:
+            self.result -= var
+        else:
+          self.result -= num
     return self
 
+print MathDojo().add(2).add(2, 5).subtract(3, 2).result
+print MathDojo().add([1],3,4).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract(2, [2,3], [1.1, 2.3]).result
+#print MathDojo().subtract(-2, [2,3], (1.1+2.3)).result
 
-#print MathDojo().add(2).add(2, 5).subtract(3, 2).result
-#print MathDojo().add([1],3,4).result
-#print MathDojo().add(1, 2).subtract(2, [2, 2]).result
-print MathDojo().subtract(2, [2,3], (1.1+2.3)).result
-#-2-(2+3)-(1.1+2.3)
-
-#print MathDojo().add([1],3,4).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract(2, [2,3], [1.1, 2.3]).result
-#print MathDojo().add([1],3,4).result
-print MathDojo().subtract(2, [2, 3]).result
-#print MathDojo().add([1],3,4).subtract(2, [2,3]).result
-
-#print MathDojo().add([1],3,4).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract(2, [2,3], [1.1, 2.3]).result
