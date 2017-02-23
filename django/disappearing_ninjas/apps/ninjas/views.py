821c0b881b19413502d443b7d8aad111b0ed9b98
kristaprokop <krista.prokop@gmail.com>
2017-02-22 19:42:06 -0600

    fix show() param to default to none

--- django/disappearing_ninjas/apps/ninjas/views.py
+++ django/disappearing_ninjas/apps/ninjas/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render, redirect
 
+
 def index(request):
     return render(request, 'ninjas/index.html')
 
-# def show_all(request):
-#     context = {
-#         'color': 'all'
-#     }
-#     return render(request, 'ninjas/ninja.html', context)
-
-def show(request, *color):
+def show(request, color=None):
     if color:
         context = {
             'color': color,
