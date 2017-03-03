f086d05a9d488249134e8d2976819a9627484f25
kristaprokop <krista.prokop@gmail.com>
2017-03-03 16:55:16 -0600

    Missing likers

---
+++ django/dojo_secrets/apps/login/models.py
@@ -0,0 +1,52 @@
+from __future__ import unicode_literals
+import re
+from django.db import models
+import bcrypt
+
+class UserManager(models.Model):
+    def validate(self, first_name, last_name, email, password1, password2):
+        try: 
+            User.objects.get(email=email) 
+            response = "User already exists. Please log in instead."
+        except: 
+            EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+            if len(first_name) < 2 or len(last_name) <2 :
+                response = ("First and last name must be 2 or more characters.")
+            elif any(filter(str.isdigit, first_name)) or any(filter(str.isdigit, last_name)):
+                response = ("Names cannot contain numbers. Please try again.")
+            elif password1 != password2:
+                response = ("Passwords must match")
+            elif len(password1) < 8:
+                response = ("Password must be 8 or more characters")
+            elif not EMAIL_REGEX.match(email):
+                response = ("Invalid Email Address!")
+            else:
+                hashed = bcrypt.hashpw(password1, bcrypt.gensalt())
+                user = User.objects.create(first_name=first_name, last_name=last_name, email=email, password=hashed)
+                response = first_name
+                return True, response
+        return False, response
+
+    def login(self, email, password):
+        try: 
+            user = User.objects.get(email=email)
+            userPwBytes = password.encode('utf-8')
+            hashedPwBytes = user.password.encode('utf-8')
+            if hashedPwBytes == (bcrypt.hashpw(userPwBytes, hashedPwBytes)):
+                return True, user
+            else:
+                response = "Email and password don't match."
+                return False, response
+        except:
+            response = ("Email not found.")
+            return False, response
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    userManager = UserManager()
---
+++ django/dojo_secrets/apps/secrets/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from .models import Secret, SecretManager, Like, LikeManager
+from ..login.models import User, UserManager
+# Create your views here.
+def index(request):
+    if 'id' not in request.session:
+        return redirect(reverse('login:index'))
+    else: 
+        context = {
+            'secrets': Secret.objects.all().order_by('created_at')[:10],
+            'likes': Like.objects.all(),
+        }
+        return render(request, 'secrets/index.html', context)
+
+def create_secret(request, id):
+    Secret.SecretManager.create_secret(message=request.POST['message'], user_id=id)
+    return redirect(reverse('secrets:index'))
+
+def delete_secret(request, id):
+    Secret.SecretManager.delete_secret(secret_id=id)
+    return redirect(reverse('secrets:index'))
+
+def logout(request):
+    request.session.clear()
+    return redirect(reverse('login:index'))
+
+def create_like(request, user_id, message_id):
+    Like.LikeManager.create_like(user_id=user_id, message_id=message_id)
+    return redirect(reverse('secrets:index'))
