9a56802e69ee437b5bae5b467d2a726df80afb87
kristaprokop <krista.prokop@gmail.com>
2017-03-08 21:44:23 -0600

    I don't remember

--- django/user_dashboard/user_dashboard/urls.py
+++ django/user_dashboard/user_dashboard/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
-from django.contrib import admin
+from apps.loginReg.models import User
+from apps.dashboard.models import Message, Comment
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
